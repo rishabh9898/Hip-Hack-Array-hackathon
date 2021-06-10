4da98ae7fab1f3fe89b95ad80d6ac9910c31d86f
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-06-10 17:04:59 +0400

    Added crsf token

--- Backend/App/hiphack/pdfreader/urls.py
+++ Backend/App/hiphack/pdfreader/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('your-name/', views.index, name='index'),
+
 ]
--- Backend/App/hiphack/pdfreader/views.py
+++ Backend/App/hiphack/pdfreader/views.py
@@ -5,6 +5,26 @@ from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget, Admin
 from django.contrib.auth.models import User
 import csv
 from django.shortcuts import render, redirect
+from .forms import NameForm
+
 
 def index(request):
     return render(request,'pdfreader/landing.html')
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'landing.html', {'form': form})
