0524a8bbf8e783ed4b76b2fbb8c0b3c28085c273
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-06-10 15:55:19 +0400

    Migrations to add to database

--- Backend/App/hiphack/pdfreader/models.py
+++ Backend/App/hiphack/pdfreader/models.py
@@ -1,13 +1,18 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Reader(models.Model):
-	name = models.CharField("Please Enter your Full Name",max_length=50, blank=True, null=True)
+	search = models.CharField("provide a brief one liner about your research.",max_length=50, blank=True, null=True)
 	pub_date = models.DateTimeField('date published')
 	email=models.EmailField(max_length=200)
+	name = models.CharField("Enter your full name please !", max_length = 100 , blank= True , null = False)
 	
 	# export_to_CSV = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.name
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
