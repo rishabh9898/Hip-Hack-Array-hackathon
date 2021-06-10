d655c4a0011e3a12a56c4477b16c69cc4166f877
rishabh9898 <rishabh.ruhela@ucalgary.ca>
2021-06-10 16:05:38 +0400

    Added Change option in Admin

--- Backend/App/hiphack/pdfreader/models.py
+++ Backend/App/hiphack/pdfreader/models.py
@@ -7,7 +7,7 @@ class Reader(models.Model):
 	search = models.CharField("provide a brief one liner about your research.",max_length=50, blank=True, null=True)
 	pub_date = models.DateTimeField('date published')
 	email=models.EmailField(max_length=200)
-	name = models.CharField("Enter your full name please !", max_length = 100 , blank= True , null = False)
+	name = models.CharField("Enter your full name please !", max_length = 100)
 	
 	# export_to_CSV = models.BooleanField(default=False)
 
