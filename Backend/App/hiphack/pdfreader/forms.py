d7e0d290a1aa3763720c127cfd0f681ec3605ee9
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2021-06-15 13:54:11 +0400

    Removed DatePublished from form

--- Backend/App/hiphack/pdfreader/forms.py
+++ Backend/App/hiphack/pdfreader/forms.py
@@ -6,6 +6,6 @@ class CreateForm(forms.ModelForm):
    export_to_CSV = forms.BooleanField(required=False)
    class Meta:
      model = Reader
-     fields = ['search','name','amount','pub_date']
+     fields = ['search','name','amount']
 # 	email=forms.CharField(error_messages={'required':'Enter your email'})
 # 	password= forms.CharField(widget=forms.PasswordInput,error_messages={'required':'Enter your password'})
