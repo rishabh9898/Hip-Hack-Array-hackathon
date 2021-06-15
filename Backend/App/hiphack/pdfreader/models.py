7a8c1655414ab5ba932f8fe2f8d05e85cc05d570
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2021-06-15 13:53:07 +0400

    Removed Date published because it was causing issues

--- Backend/App/hiphack/pdfreader/models.py
+++ Backend/App/hiphack/pdfreader/models.py
@@ -11,7 +11,7 @@ from embed_video.fields import EmbedVideoField
 
 class Reader(models.Model):
 	search = models.CharField("provide a brief one liner about your research.",max_length=50, blank=True, null=True)
-	pub_date = models.DateTimeField('date published')
+	# pub_date = models.DateTimeField('date published')
 	email=models.EmailField(max_length=200)
 	name = models.CharField("Please Enter your Full Name",max_length=50, blank=True, null=True)
 	amount = models.IntegerField("Papers you need",default='0', blank=True, null=True)
@@ -21,6 +21,6 @@ class Reader(models.Model):
 	def __str__(self):
 		return self.name
 
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+	# def was_published_recently(self):
+		# return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
