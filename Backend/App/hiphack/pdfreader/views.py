6ddfa139526fc13d72b95358bdc1023df875aaa4
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2021-06-18 19:35:40 +0400

    Update views.py
    Added pdf reader

--- Backend/App/hiphack/pdfreader/views.py
+++ Backend/App/hiphack/pdfreader/views.py
@@ -12,7 +12,9 @@ import torch
 from sentence_transformers import util
 import PyPDF2
 import os
+from PyPDF2 import PdfFileReader
 import json
+import re,string
 
 
 #Define paper data class somewhere
@@ -20,36 +22,24 @@ class Paper_Data:
 	description = ""
 	similarity = ""
 	organization = ""
-	def __init__(self, description, similarity, organization):
+	url = ""
+	count = ""
+	def __init__(self, description, similarity, organization,url,count):
 		self.description = description
 		self.similarity = similarity
 		self.organization = organization
+		self.url = url
+		self.count = count
 
 
 def index(request):
 	return render(request,'pdfreader/landing.html')
 
-# def get_name(request):
-#     # if this is a POST request we need to process the form data
-#     if request.method == 'POST':
-#         # create a form instance and populate it with data from the request:
-#         form = NameForm(request.POST)
-#         # check whether it's valid:
-#         if form.is_valid():
-#             # process the data in form.cleaned_data as required
-#             # ...
-#             # redirect to a new URL:
-#             return HttpResponseRedirect('/thanks/')
-
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = NameForm()
-
-#     return render(request, 'landing.html', {'form': form})
 charities = []
 
 def search_papers(title,model,corpus_embeddings,papers):
   query_embedding = model.encode(title+'[SEP]', convert_to_tensor=True)
+
   data_dict = {}
 
   count = 0
@@ -57,53 +47,54 @@ def search_papers(title,model,corpus_embeddings,papers):
   search_hits = util.semantic_search(query_embedding, corpus_embeddings)
   search_hits = search_hits[0]  #Get the hits for the first query
   paper_list = []
+  match_list =[]
 
-  print("Query:", title)
-  print("\nMost similar papers:")
+  # print("Query:", title)
+  # print("\nMost similar papers:")
   for hit in search_hits:
   	count+=1
   	related_paper = papers[hit['corpus_id']]
   	print(str(count)+". "+related_paper['title'])
-  	print(related_paper['abstract'])
-  	print(related_paper['url'])
-  	x = Paper_Data(related_paper['abstract'],str(format(search_hits[count-1]['score'],".2f")),related_paper['title'])
-  	paper_list.append(x)
-  	data_dict['paper_list'] = paper_list
+  	# print(related_paper['abstract'])
+  	# print(related_paper['url']+".pdf")
+  	x = Paper_Data(related_paper['abstract'],str(format(search_hits[count-1]['score'],".2f")),related_paper['title'],related_paper['url']+".pdf",str(count))
+  	if count<4:
+  		match_list.append(x)
+  	else:
+  		paper_list.append(x)
+  data_dict['paper_list'] = paper_list
+  data_dict['match_list'] = match_list
+
+  print(len(match_list),len(paper_list))
   return data_dict
 
 
-
-# def search_papers(title,model,corpus_embeddings,papers,df):  #consider adding request parameter here
-#    data_dict = {} # this is the means through which the data is sent to the result template html #definitely need to convert this into a classs
-#    query_embedding = model.encode('[CLS]'+title+'[SEP]', convert_to_tensor=True) # Converts to tensor
-#    search_hits = util.semantic_search(query_embedding, corpus_embeddings)
-#    search_hits = search_hits[0]
-#    count = 0
-#    paper_list = []
-
-#    for hit in search_hits:
-#          related_paper = papers[hit['corpus_id']]
-#          count += 1
-#          print(str(count)+ ")" + " Description of charity : " +related_paper)
-#          print("similiarity score of " + str(format(search_hits[count-1]['score'],".2f")))
-#          subsetDataFrame = df[df['Description'] == related_paper]
-#          k=subsetDataFrame.values
-#          a_string = "Name of Organization"
-#          print("\033[1m" + a_string + "\033[0m")
-#          print(str(k[0][1]))
-#          x = Paper_Data(related_paper, str(format(search_hits[count-1]['score'],".2f")),str(k[0][1]))
-#          paper_list.append(x)
-#          # data_dict['Paper'+str(count)] = x
-#     # data_dict['count'] = count
-#    data_dict['paper_list'] = paper_list
-#    return data_dict #path 'pdfreader/results.html' is not made yet, need to make the template to accept data
-
+def preprocess (text):
+  str_punctuation=string.punctuation.replace('.','')
+  text=text.lower()
+  text = re.sub(r'^https?://.[\r\n]', '', text, flags=re.MULTILINE)
+  #text = text.translate(str.maketrans('', '', str_punctuation))
+  text=" ".join(filter(lambda x:x[0]!='[', text.split()))
+  text = text.replace('\n','')
+  text= text.replace('\t','')
+  text=re.sub(' +', ' ', text)
+  return text
 
 def add_items(request):
 	if request.method =='POST':
-		form = CreateForm(request.POST)
+		form = CreateForm(request.POST, request.FILES)
 		if form.is_valid():
-			name = form.cleaned_data['name']
+			f1 = request.FILES['file1']
+			pdf = PyPDF2.PdfFileReader(f1)
+			num_pages= pdf.getNumPages()
+			text=''
+			for i in range(num_pages):
+				page=pdf.getPage(i)
+				text=text+page.extractText()
+			text = preprocess(text)
+			print(text)
+
+			# name = form.cleaned_data['name']
 			search = form.cleaned_data['search']
 
 			form.save()
@@ -111,8 +102,15 @@ def add_items(request):
 
 			# model = pickle.load(open('model_pickle','rb'))
 
-			with open("D:\\GITHUB\\rishabh9898\\hackathon-HipHack\\Backend\\App\\hiphack\\pdfreader\\paper_model","rb") as f:
+			with open("pdfreader/paper_model","rb") as f:
 				model = pickle.load(f)
+
+			with open("D:\\GITHUB\\rishabh9898\\hackathon-HipHack\\Backend\\App\\hiphack\\pdfreader\\bert_summary_model","rb") as fi:
+				model_1= pickle.load(fi)
+
+			bert_summary = ''.join(model_1(text, min_length=60))
+
+			print(bert_summary)
 
 			# df = pd.read_csv('D:\\GITHUB\\rishabh9898\\hackathon-HipHack\\Backend\\App\\hiphack\\pdfreader\\')
 			dataset_file = 'emnlp2016-2018.json'
@@ -122,14 +120,16 @@ def add_items(request):
 			with open(dataset_file) as fIn:
 				papers = json.load(fIn)
 
-			# papers = df.Description.values
+			title = bert_summary
+
 			corpus_embeddings = torch.load("D:\\GITHUB\\rishabh9898\\hackathon-HipHack\\Backend\\App\\hiphack\\pdfreader\\tensor_research_papers.pt")
-			context = search_papers(title=search,model= model,corpus_embeddings=corpus_embeddings,papers = papers)
+			context = search_papers(title=title,model= model,corpus_embeddings=corpus_embeddings,papers = papers)
 			print("rendering new page....")
 			return render(request,'pdfreader/results.html',context)
 
 
-
+		# form = CreateForm(request.POST)
+		
 
 	form = CreateForm()
 	print(" rendering same page...")
