f567eefae6753269b936319c9797ff208ab6a473
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2021-06-18 23:26:14 +0400

    Added Error.html path file

--- Backend/App/hiphack/pdfreader/urls.py
+++ Backend/App/hiphack/pdfreader/urls.py
@@ -7,6 +7,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # path('your-name/', views.index, name='index'),
-    path('pdfreader/search.html',views.add_items,name = 'add_items')
+    path('pdfreader/results.html',views.add_items,name = 'add_items'),
+    
+    path('pdfreader/search.html',views.add_items,name = 'add_items'),
+
+    path('pdfreader/error.html',views.add_items,name = 'add_items'),
+
 
 ]
