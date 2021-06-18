1264630d981efd5630af7e5e45ca5c7441eac16a
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2021-06-18 19:39:22 +0400

    Added file1(choose files option) to forms

--- Backend/App/hiphack/pdfreader/forms.py
+++ Backend/App/hiphack/pdfreader/forms.py
@@ -3,9 +3,9 @@ from .models import Reader
 from django.core import validators
 
 class CreateForm(forms.ModelForm):
-   export_to_CSV = forms.BooleanField(required=False)
-   class Meta:
-     model = Reader
-     fields = ['search','name','amount']
+	file1 = forms.FileField(widget=forms.FileInput(attrs={'class': 'file1class'}))
+	class Meta:
+		model = Reader
+		fields = ['search','file1']
 # 	email=forms.CharField(error_messages={'required':'Enter your email'})
 # 	password= forms.CharField(widget=forms.PasswordInput,error_messages={'required':'Enter your password'})
